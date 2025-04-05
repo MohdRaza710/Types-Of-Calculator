cc00da423c2b29f2ad5b4343674ca9d1effa477b
MohdRaza710 <mrmraza.710@gmail.com>
2025-04-05 18:29:29 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import image from '../assets/clac-photoroom.png'; // Adjust the path as necessary
+import image from '../../public/clac-photoroom.png'; // Adjust the path as necessary
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
